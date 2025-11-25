716bf63a191bfe3361904c3b534f01bd33ececda
willemclarke <willem.2@hotmail.com>
2025-11-25 16:34:09 +1100

    init: add simple reproduction of issue

---
+++ convex/queries.ts
@@ -0,0 +1,20 @@
+import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { paginationOptsValidator } from "convex/server";
+import { filter } from "convex-helpers/server/filter";
+
+export const paginate = query({
+  args: {
+    paginationOpts: paginationOptsValidator,
+    searchTerm: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const term = args.searchTerm?.toLowerCase();
+
+    return filter(ctx.db.query("users"), (user) => {
+      return term ? user.name.toLowerCase().includes(term) : true;
+    })
+      .order("desc")
+      .paginate(args.paginationOpts);
+  },
+});
